95c5ce03590829afa1c619d55fa01d171b88db79
AnmolTomer <anmol3540@gmail.com>
2019-02-10 23:56:25 +0530

    Add solution for 30 days of code, day 8

--- first_year/hackerrank_30_days_of_code/Python/day_8.py
+++ first_year/hackerrank_30_days_of_code/Python/day_8.py
@@ -3,3 +3,20 @@ Day - 8
 Link : https://www.hackerrank.com/challenges/30-dictionaries-and-maps/
 
 '''
+
+#  Accepted Solution
+
+entry = int(input())
+
+phone_book = {}
+for _ in range(entry):
+    name, number = input().split()
+    phone_book[name] = number
+
+query_name = input()
+while query_name:
+    if query_name in phone_book:
+        print(query_name, phone_book[query_name], sep='=')
+    else:
+        print("Not found")
+    query_name = input()
